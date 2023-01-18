a393591cf4e97404447f123be1e617f9e242e8dc
sapientlion <calmwildcat98@protonmail.com>
2023-01-18 20:45:45 +0100

    `IBlogController`: add missing methods..

--- core/interfaces/iblog_controller.php
+++ core/interfaces/iblog_controller.php
@@ -7,6 +7,9 @@ interface IBlogController
 	public function readAll(int $from, int $to) : array;
 	public function update(int $id) : bool;
 	public function delete(int $id) : bool;
+	public function validate(?Post $post = NULL) : bool;
+	public function getRowNum() : int;
+	public function run() : bool;
 }
 
 ?>
