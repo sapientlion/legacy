13ac626affc235ab69b3924aa4bf33a2eee5050e
sapientlion <calmwildcat98@protonmail.com>
2023-01-18 19:45:45 +0100

    `BlogControllerTest`: expand the class by adding more assertions and tests.

--- testing/tests/Unit/BlogControllerTest.php
+++ testing/tests/Unit/BlogControllerTest.php
@@ -59,4 +59,76 @@ class BlogControllerTest extends \Codeception\Test\Unit
 		$this->assertIsBool($result);
 		$this->assertTrue($result);
 	}
+
+	public function testBlogPostDataValidation()
+	{
+		$blogController = new BlogController();
+
+		$title = 'This is a test';
+
+		for($index = 0; $index < 11; $index++)
+		{
+			$title .= $title;
+		}
+
+		//
+		// Failure: title length is greater than given limit.
+		//
+		$result = $blogController->validate(
+			new Post($title, 'SapientLion', 'See?'));
+
+		$this->assertIsBool($result);
+		$this->assertEquals($result, false);
+
+		$title = 'SapientLion';
+
+		for($index = 0; $index < 11; $index++)
+		{
+			$title .= $title;
+		}
+
+		//
+		// Failure: author length is greater than given limit.
+		//
+		$result = $blogController->validate(
+			new Post('This is a test', $title, 'See?'));
+
+		$this->assertIsBool($result);
+		$this->assertEquals($result, false);
+
+		//
+		// Success.
+		//
+		$result = $blogController->validate(
+			new Post('This is a test', 'SapientLion', 'See?'));
+
+		$this->assertEquals($result, true);
+	}
+
+	public function testBlogPostNumberFetching()
+	{
+		$blogController = new BlogController(
+			new Post('That Dark Side of the Luna', 'SapientLion', 
+			'I wonder if someone`s still up there to play a song for me.'));
+			
+		$blogController->create();
+
+		$blogController = new BlogController(
+			new Post('The Bell That Divides...', 'SapientLion', 'I have high hopes for this project.'));
+			
+		$blogController->create();
+
+		$blogController = new BlogController(
+			new Post('Infinite River', 'SapientLion', 'No comment.'));
+			
+		$blogController->create();
+
+		$result = $blogController->getRowNum();
+
+		//
+		// Success.
+		//
+		$this->assertGreaterThan(0, $result);
+		$this->assertEquals($result, 3);
+	}
 }
