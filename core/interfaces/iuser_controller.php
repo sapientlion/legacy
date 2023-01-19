377184f3cd38357548a0f8f930a34d14d80a836b
sapientlion <calmwildcat98@protonmail.com>
2023-01-19 14:33:07 +0100

    `IUserController`: add missing class methods.

--- core/interfaces/iuser_controller.php
+++ core/interfaces/iuser_controller.php
@@ -6,8 +6,12 @@ interface IUserController
 	public function read() : bool;
 	public function update(string $currentUsername) : bool;
 	public function delete(string $currentUsername) : bool;
+	public function tryToFindMatchingEntries(string $username = '', string $email = '') : int;
+	public function validatePassword(string $password = '', string $confirmationPassword = '') : string;
 	public function signIn() : bool;
 	public function signOut() : bool;
+	public function genSiteHeader() : string;
+	public function run() : bool;
 }
 
 ?>
