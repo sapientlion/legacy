a492b84d0390f2ec1dda0cb82b8b1ad700212d5a
sapientlion <calmwildcat98@protonmail.com>
2023-02-10 16:20:24 +0100

    `UserControllerTest`: remake all test cases.

--- testing/tests/Unit/UserControllerTest.php
+++ testing/tests/Unit/UserControllerTest.php
@@ -21,243 +21,335 @@ class UserControllerTest extends \Codeception\Test\Unit
     }
 
     // tests
-	public function testUserCreation()
+	public function testUserSignupWithoutUserName()
 	{
-		//
-		// Failure: user name is not provided.
-		//
 		$userController = new UserController(
-			new User('', 'hello11@world.org', '1234567890', '1234567890'));
+			new User('', 'goodbye@world.org', '1234567890', '1234567890')
+		);
 
 		$result = $userController->create();
 
-		$this->assertIsBool($result);
 		$this->assertNotTrue($result);
+	}
 
+	public function testUserSignupWithLongUserName()
+	{
 		$username = 'Albert';
 
-		//
-		// Append the name to the variable until upper limit is reached.
-		//
 		for($index = 0; $index < 11; $index++)
 		{
 			$username .= $username;
 		}
 
-		//
-		// Failure: user name is longer than expected.
-		//
 		$userController = new UserController(
-			new User($username, 'hello11@world.org', '1234567890', '1234567890'));
+			new User($username, 'goodbye@world.org', '1234567890', '1234567890'));
 
 		$result = $userController->create();
 
 		$this->assertNotTrue($result);
+	}
 
-		//
-		// Failure: email is not provided.
-		//
+	public function testUserSignupWithoutEmail()
+	{
 		$userController = new UserController(
 			new User('Albert', '', '1234567890', '1234567890'));
 
 		$result = $userController->create();
 
 		$this->assertNotTrue($result);
+	}
 
-		//
-		// Failure: both passwords are not provided.
-		//
+	public function testUserSignupWithoutBothPasswords()
+	{
 		$userController = new UserController(
-			new User('Albert', 'hello@world.org', '', ''));
+			new User('Albert', 'goodbye@world.org', '', ''));
 
 		$result = $userController->create();
 
 		$this->assertNotTrue($result);
+	}
 
-		//
-		// Failure: password is not provided.
-		//
+	public function testUserSignupWithoutPassword()
+	{
 		$userController = new UserController(
-			new User('Albert', 'hello11@world.org', '', '1234567890'));
+			new User('Albert', 'goodbye@world.org', '', '1234567890'));
 
 		$result = $userController->create();
 
 		$this->assertNotTrue($result);
+	}
 
-		//
-		// Failure: confirmation password is not provided.
-		//
+	public function testUserSignupWithoutConfirmationPassword()
+	{
 		$userController = new UserController(
-			new User('Albert', 'hello11@world.org', '1234567890', ''));
+			new User('Albert', 'goodbye@world.org', '1234567890', ''));
 
 		$result = $userController->create();
 
 		$this->assertNotTrue($result);
+	}
 
-		//
-		// Failure: passwords are mismatched.
-		//
+	public function testUserSignupWithMismatchedPasswords()
+	{
 		$userController = new UserController(
-			new User('Albert', 'hello11@world.org', '1234567890', '12345'));
+			new User('Albert', 'goodbye@world.org', '1234567890', '12345'));
 
 		$result = $userController->create();
 
 		$this->assertNotTrue($result);
+	}
 
-		//
-		// Failure: password is shorter than expected.
-		//
+	public function testUserSignupWithShortPassword()
+	{
 		$userController = new UserController(
-			new User('Albert', 'hello11@world.org', '123', '123'));
+			new User('Albert', 'goodbye@world.org', '123', '123'));
 
 		$result = $userController->create();
 
 		$this->assertNotTrue($result);
+	}
 
-		//
-		// Failure: password is longer than expected.
-		//
+	public function testUserSignupWithLongPassword()
+	{
+		$password = '1234567890';
+
+		for($index = 0; $index < 11; $index++)
+		{
+			$password .= $password;
+		}
+
 		$userController = new UserController(
-			new User('Albert', 'hello11@world.org', '12345678901234567890123456789012345678901234567890', 
-			'12345678901234567890123456789012345678901234567890'));
+			new User('Albert', 'goodbye@world.org', $password, $password)
+		);
 
 		$result = $userController->create();
 
 		$this->assertNotTrue($result);
+	}
 
-		//
-		// Failure: user already exists.
-		//
+	public function testUserSignupWithBothAccountsBeingIdentical()
+	{
+		$username = 'Albert';
+		$email = 'goodbye@world.org';
+		$password = '1234567890';
+
 		//
 		// Create a new user account first.
 		//
 		$userController = new UserController(
-			new User('Albert', 'hello11@world.org', '1234567890', '1234567890'));
+			new User($username, $email, $password, $password)
+		);
 
 		$userController->create();
 
 		//
-		// Try to create another one with the identical information.
+		// Try to create another one with the same information.
 		//
 		$userController = new UserController(
-			new User('Albert', 'hello11@world.org', '1234567890', '1234567890'));
+			new User($username, $email, $password, $password)
+		);
 
 		$result = $userController->create();
 
 		$this->assertNotTrue($result);
+	}
 
-		//
-		// Success.
-		//
+	//
+	// Success.
+	//
+	public function testUserSignup()
+	{
 		$userController = new UserController(
-			new User('SapientLion', 'hello@world.org', '1234567890', '1234567890'));
+			new User('SapientLion', 'hello@world.org', '1234567890', '1234567890')
+		);
 		$result = $userController->create();
 		
 		$this->assertTrue($result);
 	}
 
+	public function testUserUpdateWithoutUserName()
+	{
+		$userController = new UserController(
+			new User('Albert', 'goodbye@world.org', '1234567890', '1234567890')
+		);
+
+		$userController->create();
+
+		$_SESSION['UserName'] = 'Albert';
+
+		$userController = new UserController(
+			new User('', 'goodbye@world.org', '1234567890', '1234567890')
+		);
+
+		$result = $userController->update($_SESSION['UserName']);
+
+		$this->assertNotTrue($result);
+	}
+
+	public function testUserUpdateWithLongUserName()
+	{
+		$username = 'Albert';
+
+		$userController = new UserController(
+			new User($username, 'goodbye@world.org', '1234567890', '1234567890')
+		);
+
+		$userController->create();
+
+		for($index = 0; $index < 11; $index++)
+		{
+			$username .= $username;
+		}
+
+		$_SESSION['UserName'] = 'Albert';
+
+		$userController = new UserController(
+			new User($username, 'goodbye@world.org', '1234567890', '1234567890')
+		);
+
+		$result = $userController->update($_SESSION['UserName']);
+
+		$this->assertNotTrue($result);
+	}
+
+	public function testUserUpdateWithoutEmail()
+	{
+		$userController = new UserController(
+			new User('Albert', 'goodbye@world.org', '1234567890', '1234567890')
+		);
+
+		$userController->create();
+
+		$_SESSION['UserName'] = 'Albert';
+
+		$userController = new UserController(
+			new User($_SESSION['UserName'], '', '1234567890', '1234567890')
+		);
+
+		$result = $userController->update($_SESSION['UserName']);
+
+		$this->assertNotTrue($result);
+	}
+
+	public function testUserUpdateWithoutPassword()
+	{
+		$password = '1234567890';
+
+		$userController = new UserController(
+			new User('Albert', 'goodbye@world.org', $password, $password)
+		);
+
+		$userController->create();
+
+		$_SESSION['UserName'] = 'Albert';
+
+		$userController = new UserController(
+			new User($_SESSION['UserName'], 'goodbye@world.org', '', $password)
+		);
+
+		$result = $userController->update($_SESSION['UserName']);
+
+		$this->assertNotTrue($result);
+	}
+
+	public function testUserUpdateWithoutConfirmationPassword()
+	{
+		$password = '1234567890';
+
+		$userController = new UserController(
+			new User('Albert', 'goodbye@world.org', $password, $password)
+		);
+
+		$userController->create();
+
+		$_SESSION['UserName'] = 'Albert';
+
+		$userController = new UserController(
+			new User($_SESSION['UserName'], 'goodbye@world.org', $password, '')
+		);
+
+		$result = $userController->update('Albert');
+
+		$this->assertNotTrue($result);
+	}
+
+	public function testUserUpdateWithMismatchedPasswords()
+	{
+		$password = '1234567890';
+
+		$userController = new UserController(
+			new User('Albert', 'goodbye@world.org', $password, $password)
+		);
+
+		$userController->create();
+
+		$_SESSION['UserName'] = 'Albert';
+
+		$userController = new UserController(
+			new User($_SESSION['UserName'], 'goodbye@world.org', $password, '12345')
+		);
+
+		$result = $userController->update($_SESSION['UserName']);
+
+		$this->assertNotTrue($result);
+	}
+
+	public function testUserUpdateWithShortPassword()
+	{
+		$password = '1234567890';
+
+		$userController = new UserController(
+			new User('Albert', 'hello11@world.org', $password, $password)
+		);
+
+		$userController->create();
+
+		$_SESSION['UserName'] = 'Albert';
+		$password = '123';
+
+		$userController = new UserController(
+			new User($_SESSION['UserName'], 'hello11@world.org', $password, $password)
+		);
+
+		$result = $userController->update($_SESSION['UserName']);
+
+		$this->assertNotTrue($result);
+	}
+
+	public function testUserUpdateWithLongPassword()
+	{
+		$password = '1234567890';
+
+		$userController = new UserController(
+			new User('Albert', 'hello11@world.org', $password, $password)
+		);
+
+		$userController->create();
+
+		for($index = 0; $index < 11; $index++)
+		{
+			$password .= $password;
+		}
+
+		$_SESSION['UserName'] = 'Albert';
+
+		$userController = new UserController(
+			new User($_SESSION['UserName'], 'hello11@world.org', $password, $password)
+		);
+
+		$result = $userController->update($_SESSION['UserName']);
+
+		$this->assertNotTrue($result);
+	}
+
+	//
+	// Success.
+	//
 	public function testUserUpdate()
 	{
 		//
-		// Failure: user name is not provided.
-		//
-		$userController = new UserController(
-			new User('', 'hello11@world.org', '1234567890', '1234567890'));
-
-		$result = $userController->update('Albert');
-
-		$this->assertIsBool($result);
-		$this->assertNotTrue($result);
-
-		$username = 'Albert';
-
-		//
-		// Append the name to the variable until upper limit is reached.
-		//
-		for($index = 0; $index < 11; $index++)
-		{
-			$username .= $username;
-		}
-
-		for($index = 0; $index < 11; $index++)
-		{
-			$username .= $username;
-		}
-
-		//
-		// Failure: user name is longer than expected.
-		//
-		$userController = new UserController(
-			new User($username, 'hello11@world.org', '1234567890', '1234567890'));
-
-		$result = $userController->update('Albert');
-
-		$this->assertNotTrue($result);
-
-		//
-		// Failure: email is not provided.
-		//
-		$userController = new UserController(
-			new User('Albert', '', '1234567890', '1234567890'));
-
-		$result = $userController->update('Albert');
-
-		$this->assertNotTrue($result);
-
-		//
-		// Failure: password is not provided.
-		//
-		$userController = new UserController(
-			new User('Albert', 'hello11@world.org', '', '1234567890'));
-
-		$result = $userController->update('Albert');
-
-		$this->assertNotTrue($result);
-
-		//
-		// Failure: confirmation password is not provided.
-		//
-		$userController = new UserController(
-			new User('Albert', 'hello11@world.org', '1234567890', ''));
-
-		$result = $userController->update('Albert');
-
-		$this->assertNotTrue($result);
-
-		//
-		// Failure: passwords are mismatched.
-		//
-		$userController = new UserController(
-			new User('Albert', 'hello11@world.org', '1234567890', '12345'));
-
-		$result = $userController->update('Albert');
-
-		$this->assertNotTrue($result);
-
-		//
-		// Failure: password is shorter than expected.
-		//
-		$userController = new UserController(
-			new User('Albert', 'hello11@world.org', '123', '123'));
-
-		$result = $userController->update('Albert');
-
-		$this->assertNotTrue($result);
-
-		//
-		// Failure: password is longer than expected.
-		//
-		$userController = new UserController(
-			new User('Albert', 'hello11@world.org', '12345678901234567890123456789012345678901234567890', 
-			'12345678901234567890123456789012345678901234567890'));
-
-		$result = $userController->update('Albert');
-
-		$this->assertNotTrue($result);
-
-		//
 		// Failure: user account already exists with the same email address.
-		// Note: this assertion possibly throws a false-positive result. Therefore it is disabled.
+		// Note: this assertion possibly throws a false-positive result. Therefore it is disabled by default.
 		//
 		/*$userController = new UserController(
 			new User('Albert', 'hello@world.org', '1234567890', '1234567890'));
@@ -266,48 +358,78 @@ class UserControllerTest extends \Codeception\Test\Unit
 
 		$this->assertNotTrue($result);*/
 
+		$password = '1234567890';
+
+		$userController = new UserController(
+			new User('SapientLion', 'hello@world.org', $password, $password)
+		);
+
+		$userController->create();
+
+		$_SESSION['UserName'] = 'SapientLion';
+
 		//
 		// Success.
 		//
 		$userController = new UserController(
-			new User('LionTheSapient', 'hello@world.org'));
+			new User('LionTheSapient', 'hello@world.org', '', '')
+		);
 
-		$result = $userController->update('SapientLion');
+		$result = $userController->update($_SESSION['UserName']);
 		
 		$this->assertTrue($result);
+
+		$password = '1234567890';
+
+		$userController = new UserController(
+			new User('Albert', 'goodbye@world.org', $password, $password)
+		);
+
+		$userController->create();
+
+		$_SESSION['UserName'] = 'Albert';
 
 		//
 		// Success: both passwords are not provided.
 		//
 		$userController = new UserController(
-			new User('Albert', 'hello11@world.org', '', ''));
+			new User('Alfred', 'goodbye@world.org', '', '')
+		);
 
-		$result = $userController->update('Albert');
+		$result = $userController->update($_SESSION['UserName']);
 
 		$this->assertTrue($result);
 	}
 
+	public function testUserRemovalWithNonExistingUserName()
+	{
+		$_SESSION['UserName'] = 'Cole';
+
+		$userController = new UserController(
+			new User($_SESSION['UserName'], 'hello@world.org')
+		);
+
+		$result = $userController->delete($_SESSION['UserName']);
+
+		$this->assertNotTrue($result);
+	}
+
+	//
+	// Success.
+	//
 	public function testUserRemoval()
 	{
-		//
-		// Failure: user doesn't exist in DB.
-		//
+		$password = '1234567890';
+
 		$userController = new UserController(
-			new User('Cole', 'hello@world.org'));
-
-		$result = $userController->delete('Cole');
-
-		$this->assertIsBool($result);
-		$this->assertNotTrue($result);
-
-		//
-		// Success.
-		//
-		$userController = new UserController(
-			new User('SapientLion', 'hello@world.org', '1234567890', '1234567890'));
+			new User('SapientLion', 'hello@world.org', $password, $password)
+		);
 
 		$userController->create();
-		$result = $userController->delete('SapientLion');
+
+		$_SESSION['UserName'] = 'SapientLion';
+
+		$result = $userController->delete($_SESSION['UserName']);
 		
 		$this->assertTrue($result);
 	}
